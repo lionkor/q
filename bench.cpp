384e5d7f0b06a3ef78f8da4258a813f97e498c71
Lion Kortlepel <development@kortlepel.com>
2020-07-13 16:55:01 +0200

    Overhaul Benchmarks

--- bench.cpp
+++ bench.cpp
@@ -5,50 +5,6 @@
 #include "RingBufferQueue.h"
 #include "DoublyLinkedListQueue.h"
 
-static void BM_ArrayQueue_dequeue_1(benchmark::State& state) {
-    ArrayQueue<size_t> q;
-    for (size_t i = 0; i < 100; ++i) {
-        q.enqueue(std::move(i));
-    }
-    for (auto _ : state) {
-        (void)q.dequeue();
-        q.enqueue(60);
-    }
-}
-
-static void BM_LinkedListQueue_dequeue_1(benchmark::State& state) {
-    LinkedListQueue<size_t> q;
-    for (size_t i = 0; i < 100; ++i) {
-        q.enqueue(std::move(i));
-    }
-    for (auto _ : state) {
-        (void)q.dequeue();
-        q.enqueue(60);
-    }
-}
-
-static void BM_RingBufferQueue_dequeue_1(benchmark::State& state) {
-    RingBufferQueue<size_t, 100> q;
-    for (size_t i = 0; i < 100; ++i) {
-        q.enqueue(std::move(i));
-    }
-    for (auto _ : state) {
-        (void)q.dequeue();
-        q.enqueue(60);
-    }
-}
-
-static void BM_DoublyLinkedListQueue_dequeue_1(benchmark::State& state) {
-    DoublyLinkedListQueue<size_t> q;
-    for (size_t i = 0; i < 100; ++i) {
-        q.enqueue(std::move(i));
-    }
-    for (auto _ : state) {
-        (void)q.dequeue();
-        q.enqueue(60);
-    }
-}
-
 static void BM_ArrayQueue_peek(benchmark::State& state) {
     ArrayQueue<size_t> q;
     for (size_t i = 0; i < 100; ++i) {
@@ -89,135 +45,183 @@ static void BM_DoublyLinkedListQueue_peek(benchmark::State& state) {
     }
 }
 
-static void BM_ArrayQueue_enqueue_10(benchmark::State& state) {
+#define BM_ARRAY_Q(name, n)                             \
+    static void BM_##name##n(benchmark::State& state) { \
+        for (auto _ : state) {                          \
+            name<size_t> q;                             \
+            for (size_t k = 0; k <= n; k += n / 10) {   \
+                for (size_t i = 0; i < k; ++i) {        \
+                    q.enqueue(std::move(i));            \
+                }                                       \
+                for (size_t i = 0; i < k; ++i) {        \
+                    (void)q.dequeue();                  \
+                }                                       \
+            }                                           \
+        }                                               \
+    }                                                   \
+    BENCHMARK(BM_##name##n);
+
+#define BM_ARRAY_Q_S(name, n)                           \
+    static void BM_##name##n(benchmark::State& state) { \
+        for (auto _ : state) {                          \
+            name<size_t, n> q;                          \
+            for (size_t k = 0; k <= n; k += n / 10) {   \
+                for (size_t i = 0; i < k; ++i) {        \
+                    q.enqueue(std::move(i));            \
+                }                                       \
+                for (size_t i = 0; i < k; ++i) {        \
+                    (void)q.dequeue();                  \
+                }                                       \
+            }                                           \
+        }                                               \
+    }                                                   \
+    BENCHMARK(BM_##name##n);
+
+BM_ARRAY_Q(ArrayQueue, 10)
+BM_ARRAY_Q(ArrayQueue, 100)
+BM_ARRAY_Q(ArrayQueue, 1000)
+BM_ARRAY_Q(ArrayQueue, 10000)
+BM_ARRAY_Q(ArrayQueue, 50000)
+
+BM_ARRAY_Q(LinkedListQueue, 10)
+BM_ARRAY_Q(LinkedListQueue, 100)
+BM_ARRAY_Q(LinkedListQueue, 1000)
+BM_ARRAY_Q(LinkedListQueue, 10000)
+BM_ARRAY_Q(LinkedListQueue, 50000)
+
+BM_ARRAY_Q_S(RingBufferQueue, 10)
+BM_ARRAY_Q_S(RingBufferQueue, 100)
+BM_ARRAY_Q_S(RingBufferQueue, 1000)
+BM_ARRAY_Q_S(RingBufferQueue, 10000)
+BM_ARRAY_Q_S(RingBufferQueue, 50000)
+
+BM_ARRAY_Q(DoublyLinkedListQueue, 10)
+BM_ARRAY_Q(DoublyLinkedListQueue, 100)
+BM_ARRAY_Q(DoublyLinkedListQueue, 1000)
+BM_ARRAY_Q(DoublyLinkedListQueue, 10000)
+BM_ARRAY_Q(DoublyLinkedListQueue, 50000)
+
+
+static void BM_ArrayQueue_all(benchmark::State& state) {
     for (auto _ : state) {
         ArrayQueue<size_t> q;
-        for (size_t i = 0; i < 10; ++i) {
-            q.enqueue(std::move(i));
-        }
-    }
-}
-
-static void BM_LinkedListQueue_enqueue_10(benchmark::State& state) {
-    for (auto _ : state) {
-        LinkedListQueue<size_t> q;
-        for (size_t i = 0; i < 10; ++i) {
-            q.enqueue(std::move(i));
-        }
-    }
-}
-
-static void BM_RingBufferQueue_enqueue_10(benchmark::State& state) {
-    for (auto _ : state) {
-        RingBufferQueue<size_t, 10> q;
-        for (size_t i = 0; i < 10; ++i) {
-            q.enqueue(std::move(i));
-        }
-    }
-}
-
-static void BM_DoublyLinkedListQueue_enqueue_10(benchmark::State& state) {
-    for (auto _ : state) {
-        DoublyLinkedListQueue<size_t> q;
-        for (size_t i = 0; i < 10; ++i) {
-            q.enqueue(std::move(i));
-        }
-    }
-}
-
-static void BM_ArrayQueue_enqueue_100(benchmark::State& state) {
-    for (auto _ : state) {
-        ArrayQueue<size_t> q;
         for (size_t i = 0; i < 100; ++i) {
             q.enqueue(std::move(i));
         }
+        for (size_t i = 0; i < 100; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 1000; ++i) {
+            q.enqueue(std::move(i));
+        }
+        for (size_t i = 0; i < 1000; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 10000; ++i) {
+            q.enqueue(std::move(i));
+        }
+        for (size_t i = 0; i < 10000; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 10000; ++i) {
+            q.enqueue(std::move(i));
+        }
+        q.clear();
     }
 }
 
-static void BM_LinkedListQueue_enqueue_100(benchmark::State& state) {
+static void BM_LinkedListQueue_all(benchmark::State& state) {
     for (auto _ : state) {
         LinkedListQueue<size_t> q;
         for (size_t i = 0; i < 100; ++i) {
             q.enqueue(std::move(i));
         }
+        for (size_t i = 0; i < 100; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 1000; ++i) {
+            q.enqueue(std::move(i));
+        }
+        for (size_t i = 0; i < 1000; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 10000; ++i) {
+            q.enqueue(std::move(i));
+        }
+        for (size_t i = 0; i < 10000; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 10000; ++i) {
+            q.enqueue(std::move(i));
+        }
+        q.clear();
     }
 }
 
-static void BM_RingBufferQueue_enqueue_100(benchmark::State& state) {
+static void BM_RingBufferQueue_all(benchmark::State& state) {
     for (auto _ : state) {
-        RingBufferQueue<size_t, 100> q;
+        RingBufferQueue<size_t, 10000> q;
         for (size_t i = 0; i < 100; ++i) {
             q.enqueue(std::move(i));
         }
+        for (size_t i = 0; i < 100; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 1000; ++i) {
+            q.enqueue(std::move(i));
+        }
+        for (size_t i = 0; i < 1000; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 10000; ++i) {
+            q.enqueue(std::move(i));
+        }
+        for (size_t i = 0; i < 10000; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 10000; ++i) {
+            q.enqueue(std::move(i));
+        }
+        q.clear();
     }
 }
 
-static void BM_DoublyLinkedListQueue_enqueue_100(benchmark::State& state) {
+static void BM_DoublyLinkedListQueue_all(benchmark::State& state) {
     for (auto _ : state) {
         DoublyLinkedListQueue<size_t> q;
         for (size_t i = 0; i < 100; ++i) {
             q.enqueue(std::move(i));
         }
-    }
-}
-
-static void BM_ArrayQueue_enqueue_100000(benchmark::State& state) {
-    for (auto _ : state) {
-        ArrayQueue<size_t> q;
-        for (size_t i = 0; i < 100000; ++i) {
+        for (size_t i = 0; i < 100; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 1000; ++i) {
             q.enqueue(std::move(i));
         }
-    }
-}
-
-static void BM_LinkedListQueue_enqueue_100000(benchmark::State& state) {
-    for (auto _ : state) {
-        LinkedListQueue<size_t> q;
-        for (size_t i = 0; i < 100000; ++i) {
+        for (size_t i = 0; i < 1000; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 10000; ++i) {
             q.enqueue(std::move(i));
         }
-    }
-}
-
-static void BM_RingBufferQueue_enqueue_100000(benchmark::State& state) {
-    for (auto _ : state) {
-        RingBufferQueue<size_t, 100000> q;
-        for (size_t i = 0; i < 100000; ++i) {
+        for (size_t i = 0; i < 10000; ++i) {
+            (void)q.dequeue();
+        }
+        for (size_t i = 0; i < 10000; ++i) {
             q.enqueue(std::move(i));
         }
+        q.clear();
     }
 }
 
-static void BM_DoublyLinkedListQueue_enqueue_100000(benchmark::State& state) {
-    for (auto _ : state) {
-        DoublyLinkedListQueue<size_t> q;
-        for (size_t i = 0; i < 100000; ++i) {
-            q.enqueue(std::move(i));
-        }
-    }
-}
-
-BENCHMARK(BM_ArrayQueue_dequeue_1);
-BENCHMARK(BM_LinkedListQueue_dequeue_1);
-BENCHMARK(BM_RingBufferQueue_dequeue_1);
-BENCHMARK(BM_DoublyLinkedListQueue_dequeue_1);
+BENCHMARK(BM_ArrayQueue_all);
+BENCHMARK(BM_LinkedListQueue_all);
+BENCHMARK(BM_RingBufferQueue_all);
+BENCHMARK(BM_DoublyLinkedListQueue_all);
 
 BENCHMARK(BM_ArrayQueue_peek);
 BENCHMARK(BM_LinkedListQueue_peek);
 BENCHMARK(BM_RingBufferQueue_peek);
 BENCHMARK(BM_DoublyLinkedListQueue_peek);
-
-BENCHMARK(BM_ArrayQueue_enqueue_10);
-BENCHMARK(BM_LinkedListQueue_enqueue_10);
-BENCHMARK(BM_RingBufferQueue_enqueue_10);
-BENCHMARK(BM_DoublyLinkedListQueue_enqueue_10);
-BENCHMARK(BM_ArrayQueue_enqueue_100);
-BENCHMARK(BM_LinkedListQueue_enqueue_100);
-BENCHMARK(BM_RingBufferQueue_enqueue_100);
-BENCHMARK(BM_DoublyLinkedListQueue_enqueue_100);
-BENCHMARK(BM_ArrayQueue_enqueue_100000);
-BENCHMARK(BM_LinkedListQueue_enqueue_100000);
-BENCHMARK(BM_RingBufferQueue_enqueue_100000);
-BENCHMARK(BM_DoublyLinkedListQueue_enqueue_100000);
 
 BENCHMARK_MAIN();
